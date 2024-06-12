425a8edcd4df57e485b85ad673e2ff05299bd403
Sonalazar5 <sonalazar5@gmail.com>
2024-06-12 21:22:21 +0530

    COMPLETED

---
+++ src/components/Add.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">TURF-BOOKING</h5>
+
+    <label htmlFor="" className="form-label">NAME</label>
+    <input type="text" className="form control" />
+    <label htmlFor="" className="form-label">PASSWORD</label>
+    <input type="text" className="form control" />
+
+<h3>DIDN'T SIGNUP YET?</h3>
+    <a href="#" class="btn btn-primary">SIGN-UP</a>
+  </div>
+</div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Add
